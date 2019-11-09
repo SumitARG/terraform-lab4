44caf78f828b2930d7b919c757cffa5640567b8e
SumitARG <42317660+SumitARG@users.noreply.github.com>
2019-11-09 15:05:50 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -15,3 +15,10 @@ variable "webapplocs" {
     type        = "list"
     default     = []
 }
+variable "clientsecret"{
+    default = "Jg-xsz_p7Ru/5TOiRke_F1fsimnVUVJ8"
+}
+
+variable "subscriptionid"{
+    default = "32c32163-f026-41ed-9503-3b0ee6ec5540"
+}
